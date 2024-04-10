0160e9d6d6b7b3e7ffe4d09b422970363120d550
jvpatil <49668147+jvpatil@users.noreply.github.com>
2024-04-10 21:44:40 +0530

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -6,6 +6,6 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 YT_GenM3U_LiveTV.py > ../yt_livetv.m3u
+python3 YT_GenM3U_LiveTV.py > ../YouTube_LiveTVs.m3u
 
 echo m3u grabbed
